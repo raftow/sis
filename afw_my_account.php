d876413cb59c661f1740277c13bfb98bea42f029
rafiq boubaker <rafiq.boubaker@gmail.com>
2023-11-18 07:47:06 +0300

    some fixes

--- afw_my_account.php
+++ afw_my_account.php
@@ -1,42 +1,44 @@
 <?php
+
+//die("this page is obsolete to reimplement using MVC");
 $file_dir_name = dirname(__FILE__);
+$SPECIALITY_EXPERT_ID = 162;
 // set_time_limit(8400);
 // ini_set('error_reporting', E_ERROR | E_PARSE | E_RECOVERABLE_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR);
 
 //// here old require of common.php
-//require_once("$file_dir_name/../cms/menu_functions.php");
-require_once("$file_dir_name/../external/db.php");  
-// require_once student.php");
+/*
+require_once("$file_dir_name/../cms/menu_functions.php");
+require_once("$file_dir_name/../../external/db.php");  
 
 
 
 
+*/
+require_once("afw_autoloader.php");
+require_once("$file_dir_name/../../external/db.php");  
+
+
+
+require_once("afw_session.php");
 AfwSession::startSession();
-$me = $_SESSION["user_id"];
-$email = $_SESSION["user_email"];
-
-
-
-$home_path_browser = $menu_title." &larr; ".$path_title;
+// $home_path_browser = $menu_title." &larr; ".$path_title;
 $force_allow_access_to_customers = true;
 
 $check_depending_user_type="NO-CHECK";
 
-
-$objToShow = new Student();
-$objToShow->load($me-5000000);
-$ro = true;
-if($objToShow and $objToShow->getId() and (!$ro))
+$r = "control";
+// die("here rafik 20210321 - 0");
+$objToShow = AfwSession::getUserConnected();
+if($objToShow and $objToShow->isAdmin() and false)
 {
-        
-        include_once ("$file_dir_name/ini.php");
-        include_once ("$file_dir_name/module_config.php");
-        $Main_Page = "afw_mode_edit.php";
-        $My_Module = "pag";
-        $cl = "Student";
-        $currmod="sis";
-        $id = $objToShow->getId();
-        include("$file_dir_name/../lib/afw/afw_main_page.php");
+        die("here rafik 20210321");
+        $_GET["Main_Page"] = "afw_mode_edit.php";
+        $_GET["My_Module"] = "pag";
+        $_GET["cl"] = "Auser";
+        $_GET["currmod"]="ums";
+        $_GET["id"] = $objToShow->getId();
+        include("main.php");
         
         /*
         $Main_Page = "my_account.php";
@@ -44,15 +46,18 @@ if($objToShow and $objToShow->getId() and (!$ro))
         include_once ("$file_dir_name/ini.php");
         include_once ("$file_dir_name/module_config.php");
         
-        include("$file_dir_name/../lib/afw/afw_main_page.php");*/
+        include("afw_main_page.php");*/
+}
+elseif($objToShow)
+{
+        $direct_dir_name = dirname(__FILE__); 
+        $Direct_Page = "show_object.php";
+        //die("$Direct_Page to run under$file_dir_name/../$MODULE/afw_direct_page.php");
+        include("afw_direct_page.php");
+        
 }
 else
 {
-        include_once ("$file_dir_name/ini.php");
-        include_once ("$file_dir_name/module_config.php");
-
-        $direct_dir_name = "$file_dir_name/../lib/afw"; 
-        $Direct_Page = "show_object.php";
-        include("$file_dir_name/../lib/afw/afw_direct_page.php");
+        echo "<div class='error'>No account object found, You need to login before</div>";
 }
 ?>
