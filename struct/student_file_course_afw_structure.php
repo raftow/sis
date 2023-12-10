13ee5b3ec939f1e788e3123be3b0ff2ec9cedf9f
rafiq boubaker <rafiq.boubaker@gmail.com>
2023-12-10 06:49:25 +0300

    fix bugs on sfc

--- struct/student_file_course_afw_structure.php
+++ struct/student_file_course_afw_structure.php
@@ -10,6 +10,7 @@ class SisStudentFileCourseAfwStructure
             $context_cols = "";
 
             $obj->QEDIT_MODE_NEW_OBJECTS_DEFAULT_NUMBER = 15;
+            $obj->QEDIT_SUBMIT_BTN_EACH_RECORD = 1;
             // $obj->HIDE_DISPLAY_MODE = true;
             // $obj->hideQeditCommonFields = true;
             $obj->DISPLAY_FIELD = "";
