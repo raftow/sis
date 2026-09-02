991d7dbe1033519c3ba923171c2129c68245fc98
raftow <rboubaker@tvtc.gov.sa>
2026-09-02 11:17:20 +0300

    work on workflow - etl - crm - store - projects

--- models/assass2.php
+++ models/assass2.php
@@ -760,9 +760,9 @@ class Assass2 extends SisObject
                 }
 
 
-                $sql .= " \n insert into STUDENTS.TRANSACTION_WS(ID,CREATED_AT,STUDENTUNIQUEID,TRANS_NO,INPUT_PAYLOAD,OUTPUT_PAYLOAD,STATUS) 
+                $sql .= " \n insert into STUDENTS.TRANSACTION_WS(CREATED_AT,STUDENTUNIQUEID,TRANS_NO,INPUT_PAYLOAD,OUTPUT_PAYLOAD,STATUS) 
                              values 
-                             ($id_tr_ws, CURRENT_TIMESTAMP, $student_unique_id, $trans_no, 'excel line $row_num', '$output_message', '$tr_status'); \n";
+                             (CURRENT_TIMESTAMP, '$student_unique_id', '$trans_no', 'excel line $row_num', '$output_message', '$tr_status'); \n";
 
                 $sql .= "\n\t commit;\n";
 
